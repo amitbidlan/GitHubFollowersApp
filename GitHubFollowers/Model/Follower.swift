d0dc42bd15985b563bd0561c2293b8119e83eef8
amitbidlan <asianbarrow@gmail.com>
2024-12-18 00:32:05 +0900

    Part 1 - Models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Amit Bidlan on 2024/12/17.
+//
+
+import Foundation
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Amit Bidlan on 2024/12/18.
+//
+
+import Foundation
