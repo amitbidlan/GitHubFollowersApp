afdb15dac8d61cc26505074505ca7b2975353e0f
amitbidlan <asianbarrow@gmail.com>
2024-12-20 16:48:00 +0900

    Refactored UIVIew in HelperKit

---
+++ GitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GitHubFollowers
+//
+//  Created by Amit Bidlan on 2024/12/20.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view:UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding:CGFloat = 12
+        let minimumItemSpacing:CGFloat = 10
+        
+        let availableWidth:CGFloat = width - (padding*2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth/3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
