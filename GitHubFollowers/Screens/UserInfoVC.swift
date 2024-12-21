23feb434f242c9f355ed685c7854ed9184500113
amitbidlan <asianbarrow@gmail.com>
2024-12-21 17:43:59 +0900

    Implemented Info VC (Header)

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -10,7 +10,8 @@ import UIKit
 class UserInfoVC: UIViewController {
     
     var username: String!
-
+    let headerView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -21,21 +22,40 @@ class UserInfoVC: UIViewController {
             guard let self = self else {return}
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async{
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
                 
             case .failure(let error):
                 self.presentGFAlertonMainThread(title: "Some Error", message: error.rawValue, buttonTitle: "OK")
             }
             
         }
+        layoutUI()
         
+    }
+    
+    func layoutUI(){
+        view.addSubview(headerView)
+        //headerView.backgroundColor = .lightGray
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
     }
     
     @objc func dismissVC(){
         dismiss(animated: true)
     }
     
-
-   
-
+    func add(childVC:UIViewController, to containerView: UIView){
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
+    
 }
