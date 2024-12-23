688a479c035faab4002139bb5bd093efba7d7203
amitbidlan <asianbarrow@gmail.com>
2024-12-23 09:07:37 +0900

    Favorites Screen implemented

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -8,23 +8,68 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
-    
-    
+    let tableView = UITableView()
+    var favorites:[Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureTableView()
+    }
+    
+    func configureViewController(){
         view.backgroundColor = .systemBackground
+        title = "お気に入り"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureTableView(){
         
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+        
+    }
+    
+    func getFavorites(){
         PersistenceManager.retreiveFavorites{
-            result in
+           [weak self] result in
+            guard let self = self else {return}
             switch result {
             case .success(let favorites):
-                print(favorites)
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "お気に入りがマークされていません。\nお気に入りを追加してください。", in: self.view)
+                }else{
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                self.favorites = favorites
             case .failure(let error):
-                break
+                self.presentGFAlertonMainThread(title: "何か問題が発生しました。", message: error.rawValue, buttonTitle: "閉じる")
             }
         }
     }
-    
 
+}
+
+
+extension FavoritesListVC:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
+    
 }
